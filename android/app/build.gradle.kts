f8bb5e0fd47b87e51a91e914f3333edb660b1e6a
eslam-9 <eslamage20@gmail.com>
2026-08-08 07:44:38 +0300

    chore(android): bump version to 2.1.0 (versionCode 7)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -57,8 +57,8 @@ android {
         applicationId = "com.mafioso.game"
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 6
-        versionName = "2.0.1"
+        versionCode = 7
+        versionName = "2.1.0"
         multiDexEnabled = true
     }
 
