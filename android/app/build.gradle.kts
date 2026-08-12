1a3341e4311eb355fe5b15f5c4c22296e38ddac7
eslam-9 <eslamage20@gmail.com>
2026-08-12 05:58:41 +0300

    play store req

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -56,9 +56,9 @@ android {
     defaultConfig {
         applicationId = "com.mafioso.game"
         minSdk = flutter.minSdkVersion
-        targetSdk = 35
-        versionCode = 7
-        versionName = "2.1.0"
+        targetSdk = flutter.targetSdkVersion
+        versionCode = 8
+        versionName = "2.2.0"
         multiDexEnabled = true
     }
 
